3f7799c2134c4993ec766633a721a9217d7742d4
BerNie Lin <>
2021-04-21 15:31:08 +0000

    Fix the multiple of 3 problem and use Function.

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-  
-# -*- coding: UTF-8 -*-
-#BerNie_test
-# Filename : test.py
-# author by : www.twcode01.com 
-# Python 程式用於檢測使用者輸入的數值是否為質數
- 
-# 使用者輸入數值
-num = int(input("input number : "))
-while(num >= 0):
-# 質數大於 1
-  if num > 1:
-   # 檢視因子
-   for i in range(2,num):
-    if (num % i) == 0:
-      num1 = num%i
-      print(num,"不是質數")        
-      print(i,"X",num//i,"=",num)
-      num = int(input("input number : "))
+def Prime(n):
+  c = 2
+  while c < n:
+    if n % c == 0:
+      print('Not Prime.')
       break
-    else :
-      print(num,"是質數")
-      num = int(input("input number : "))
-      break
-    
+    c += 1
+  if c == n:
+    print('Is prime.')
+n = 1
+while(n >= 0):
+  n = int(input('intput number (Enter:=<0 exit.) :'))
+  Prime(n)
