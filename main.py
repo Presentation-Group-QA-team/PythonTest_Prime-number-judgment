0bc1a59da943360dc283d811d3ce496b775363fe
BerNie Lin <>
2021-04-21 09:09:50 +0000

    + Name

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # -*- coding: UTF-8 -*-
- 
+#BerNie_test
 # Filename : test.py
-# author by : www.twcode01.com
- 
+# author by : www.twcode01.com 
 # Python 程式用於檢測使用者輸入的數值是否為質數
  
 # 使用者輸入數值
-num = int(input("請輸入一個數值: "))
+num = int(input("input number : "))
  
 # 質數大於 1
 if num > 1:
@@ -14,7 +13,7 @@ if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
            print(num,"不是質數")
-           print(i,"乘於",num//i,"是",num)
+           print(i,"X",num//i,"=",num)
            break
    else:
        print(num,"是質數")
@@ -22,3 +21,4 @@ if num > 1:
 # 如果輸入的數值小於或等於 1，不是質數
 else:
    print(num,"不是質數")
+
