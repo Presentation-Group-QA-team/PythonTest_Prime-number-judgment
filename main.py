971ad099dfa1ff553989b744ff5fe06815b5c9b5
BerNie Lin <>
2021-04-21 09:46:31 +0000

    while_test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+  
 # -*- coding: UTF-8 -*-
 #BerNie_test
 # Filename : test.py
@@ -6,19 +7,19 @@
  
 # 使用者輸入數值
 num = int(input("input number : "))
- 
+while(num >= 0):
 # 質數大於 1
-if num > 1:
+  if num > 1:
    # 檢視因子
    for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"不是質數")
-           print(i,"X",num//i,"=",num)
-           break
-   else:
-       print(num,"是質數")
-       
-# 如果輸入的數值小於或等於 1，不是質數
-else:
-   print(num,"不是質數")
-
+    if (num % i) == 0:
+      num1 = num%i
+      print(num,"不是質數")        
+      print(i,"X",num//i,"=",num)
+      num = int(input("input number : "))
+      break
+    else :
+      print(num,"是質數")
+      num = int(input("input number : "))
+      break
+    
